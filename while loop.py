c9f021ef1e28a44d5ec88dd9d2a93885b91a7278
Pradeep Kumar <82723874+Pradeep1519@users.noreply.github.com>
2022-09-01 22:48:13 +0530

    WHILE LOOP

---
+++ while loop.py
@@ -0,0 +1,16 @@
+# start
+# condition
+# increment\decriment
+
+i = 1  # start
+while i <= 10:  # condition
+    print(i, "hello pradeep")  # statement
+    i = i + 1  # increment
+print(i)  # print statement
+print()
+print()
+a = 10  # strat
+while a >= 1:  # condition
+    print("welcome to the coding world")  # print statement
+    a = a - 1  # decrement
+    print(a)  # print statement
