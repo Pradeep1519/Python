ef5ae32f9335958e87ab7f5381a434bdcaa4afbe
Pradeep Kumar <82723874+Pradeep1519@users.noreply.github.com>
2022-09-01 22:43:02 +0530

    INPUT FUNCTION EXAMPLES

---
+++ type casting.py
@@ -0,0 +1,11 @@
+a = int(input("enter the value1=> "))  # type cast (int) is the only intiger value consider and exicute
+b = int(input("enter the value2=> "))  # type cast (int)
+print(a + b)
+
+a = float(input("enter the value1=> "))  # type cast (float) is consider the floating value and input the intiger value tha the floating
+b = float(input("enter the value2=> "))  # type cast (float) type casting function convert to the floating value than exicute
+print(a + b)
+
+a = eval(input("enter the value1=> "))  # type cast (eval) is the consider the binary and int and floating value
+b = eval(input("enter the value2=> "))  # type cast (eval)
+print(a + b)
